f50c5c58773b739b44cd12e947823dd2a2d4f61b
Solen Dogan <solendogan.sap@gmail.com>
2019-09-06 21:16:35 +0300

    Add files via upload

---
+++ Test/Driver.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package test;
+
+import Strategy.*;
+
+/**
+ * @author User
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+     RedHeadDuck _redHeadDuck = new RedHeadDuck();
+     _redHeadDuck.fly( );
+     _redHeadDuck.display();
+	}
+
+}
